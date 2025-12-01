316fe9422f1e036cc5e3953353e7406f1584c976
Nikhil2005Rai <n2005rai@gmail.com>
2025-12-01 11:48:03 +0530

    complete trpc setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,9 @@
-import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
+import prisma from '@/lib/db';
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
+  getUsers: baseProcedure
     .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
+      return prisma.user.findMany();
     }),
 });
 // export type definition of API
