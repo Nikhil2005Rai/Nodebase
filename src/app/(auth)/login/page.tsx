aa07b8dfabde1baf120d3aeb5db1b18fa6c4b400
Nikhil2005Rai <n2005rai@gmail.com>
2025-12-02 15:56:55 +0530

    add login ui

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/features/auth/login-form";
+
+const Page = () => {
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    )
+}
+
+export default Page;
