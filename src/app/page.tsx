10a3a76bb36c68053681fb1c5754c17553617ffd
Nikhil2005Rai <n2005rai@gmail.com>
2025-12-03 23:10:40 +0530

    added trpc protectedBaseProcidure for safety in case of requireAuth() break

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth.utils";
+import { caller } from "@/trpc/server";
+import { LogoutButton } from "./logout";
 
 const Page = async () => {
   await requireAuth();
 
+  const data = await caller.getUsers();
+
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      protected server componet
-      
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+      protected server component
+      <div>
+        {JSON.stringify(data, null, 2)}
+      </div>
+      <LogoutButton />
     </div>
   );
 };
