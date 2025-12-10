7986483915b06c381c9a368ba154b52a137469a5
Nikhil2005Rai <n2005rai@gmail.com>
2025-12-10 18:49:09 +0530

    Add polar auth and tested it

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
 import prisma from "./db";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -10,5 +11,24 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
         autoSignIn: true,
-    }
+    },
+    plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products: [
+                        {
+                            productId: "6e3f4a17-1385-4255-a586-d59c0173428f",
+                            slug: "Nodebase-Pro" 
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true
+                }),
+                portal(),
+            ],
+        })
+    ]
 });
