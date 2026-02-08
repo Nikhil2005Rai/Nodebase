b80cd3f62d8fcb47f6ec39a4947b0ba059ff6226
Nikhil2005Rai <n2005rai@gmail.com>
2026-02-08 14:21:07 +0530

    feat: Add execution detail page with error and output display

--- src/features/executions/server/router.ts
+++ src/features/executions/server/router.ts
@@ -8,7 +8,20 @@ export const executionsRouter = createTRPCRouter({
         .input(z.object({ id: z.string() }))
         .query(({ ctx, input }) => {
             return prisma.execution.findUniqueOrThrow({
-                where: { id: input.id, workflow: { userId: ctx.auth.user.id } },
+                where: { 
+                    id: input.id, 
+                    workflow: { 
+                        userId: ctx.auth.user.id 
+                    },
+                },
+                include: {
+                    workflow: {
+                        select: {
+                            id: true,
+                            name: true,
+                        }
+                    }
+                }
             });         
         }),
     getMany: protectedProcedure
